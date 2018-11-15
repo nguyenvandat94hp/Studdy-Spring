181ef54e7b6f21333751d1ef16a09465129b0d05
nguyenvandat94hp <doicodedao@gmail.com>
2018-11-15 19:04:38 +0700

    MVC-Lession8:15112018-1903: Config file HomeController

--- SpringMVC/src/main/java/vn/com/spring/controller/HomeController.java
+++ SpringMVC/src/main/java/vn/com/spring/controller/HomeController.java
@@ -1,23 +1,14 @@
 package vn.com.spring.controller;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import vn.com.spring.entity.NhanVien;
 
 @Controller
 public class HomeController {
 
 	@RequestMapping("/")
-	@ResponseBody
 	public String index() {
-		ApplicationContext context = new ClassPathXmlApplicationContext("config-spring.xml");
-		NhanVien nhanVien = (NhanVien) context.getBean("nhanVien");
-		String display = nhanVien.getGiamDoc().toString();
-		System.out.println(display);
-		return display;
+
+		return "index.jsp";
 	}
 }
