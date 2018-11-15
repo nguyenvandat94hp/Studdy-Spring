060b4faed9eab90e4d33880d643e683faa55a738
nguyenvandat94hp <doicodedao@gmail.com>
2018-11-16 00:06:30 +0700

    MVC-Lession12:16112018-0005: method receive request param from client send to server

--- SpringMVC/src/main/java/vn/com/spring/controller/HomeController.java
+++ SpringMVC/src/main/java/vn/com/spring/controller/HomeController.java
@@ -1,27 +1,22 @@
 package vn.com.spring.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import vn.com.spring.entity.NhanVien;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
-	
-	@RequestMapping("/")
-	public String home(ModelMap modeMap) {
-		modeMap.addAttribute("listNhanVien", createListNhanVien());
+
+	@RequestMapping(value = { "/", "/index" }, method = RequestMethod.GET)
+	public String index() {
 		return "index";
 	}
-	
-	private static List<NhanVien> createListNhanVien(){
-		List<NhanVien> listNhanVien = new ArrayList<NhanVien>();
-		listNhanVien.add(new NhanVien(0203, "Dat"));
-		listNhanVien.add(new NhanVien(2711, "Yen"));	
-		return listNhanVien;
+
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
+	public String hello(@RequestParam(name = "id", required = true) int id, ModelMap modeMap) {
+		modeMap.addAttribute("id", id);
+		return "hello";
 	}
 }
