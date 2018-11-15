3d03a255138838d6d179b6e71a08e789a5e0dda4
nguyenvandat94hp <doicodedao@gmail.com>
2018-11-15 23:34:22 +0700

    MVC-Lession11:15112018-2334: add list object in modeMap for send client

--- SpringMVC/src/main/java/vn/com/spring/controller/HomeController.java
+++ SpringMVC/src/main/java/vn/com/spring/controller/HomeController.java
@@ -1,30 +1,27 @@
 package vn.com.spring.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+
+import vn.com.spring.entity.NhanVien;
 
 @Controller
 public class HomeController {
-
+	
 	@RequestMapping("/")
-	public ModelAndView index() {
-		ModelAndView modelAndView = new ModelAndView();
-		modelAndView.setViewName("index");
-		String users ="User_ModelAndView";
-		int age = 18;
-		modelAndView.addObject("tendangnhap", users);
-		modelAndView.addObject("ages", age);
-		return modelAndView;
+	public String home(ModelMap modeMap) {
+		modeMap.addAttribute("listNhanVien", createListNhanVien());
+		return "index";
 	}
 	
-	@RequestMapping("/home")
-	public String home(ModelMap modeMap) {
-		String users ="User_Method_String";
-		modeMap.addAttribute("tendangnhap", users);
-		int age = 18;
-		modeMap.addAttribute("ages", age);
-		return "index";
+	private static List<NhanVien> createListNhanVien(){
+		List<NhanVien> listNhanVien = new ArrayList<NhanVien>();
+		listNhanVien.add(new NhanVien(0203, "Dat"));
+		listNhanVien.add(new NhanVien(2711, "Yen"));	
+		return listNhanVien;
 	}
 }
