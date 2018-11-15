5bd26c4b695c9c22591118ca8632ff0c2c60b2e0
nguyenvandat94hp <doicodedao@gmail.com>
2018-11-15 22:14:59 +0700

    MVC-Lession10:15112018-2214: Guide use method ModeAndView or String in Controller

--- SpringMVC/src/main/java/vn/com/spring/controller/HomeController.java
+++ SpringMVC/src/main/java/vn/com/spring/controller/HomeController.java
@@ -1,14 +1,30 @@
 package vn.com.spring.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 
 	@RequestMapping("/")
-	public String index() {
-
+	public ModelAndView index() {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("index");
+		String users ="User_ModelAndView";
+		int age = 18;
+		modelAndView.addObject("tendangnhap", users);
+		modelAndView.addObject("ages", age);
+		return modelAndView;
+	}
+	
+	@RequestMapping("/home")
+	public String home(ModelMap modeMap) {
+		String users ="User_Method_String";
+		modeMap.addAttribute("tendangnhap", users);
+		int age = 18;
+		modeMap.addAttribute("ages", age);
 		return "index";
 	}
 }
