6a9f17c7e02e7e145e154a2290640b4e45737f65
nguyenvandat94hp <doicodedao@gmail.com>
2018-11-16 10:42:43 +0700

    MVC-Lession16:16112018-1041: Controller handler data when we are using @ModeAttribute

--- SpringMVC/src/main/java/vn/com/spring/controller/HomeController.java
+++ SpringMVC/src/main/java/vn/com/spring/controller/HomeController.java
@@ -2,9 +2,11 @@ package vn.com.spring.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import vn.com.spring.entity.NhanVien;
 
 @Controller
 public class HomeController {
@@ -15,8 +17,8 @@ public class HomeController {
 	}
 
 	@RequestMapping(value = "/hello", method = RequestMethod.POST)
-	public String hello(@RequestParam String userName, ModelMap modeMap) {
-		modeMap.addAttribute("id", userName);
+	public String hello(@ModelAttribute NhanVien nhanVien, ModelMap modeMap) {
+		modeMap.addAttribute("nhanVien", nhanVien);
 		return "hello";
 	}
 }
