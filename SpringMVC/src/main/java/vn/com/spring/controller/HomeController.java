0c12e6b195d29270954805be69bae6c984cf93dd
nguyenvandat94hp <doicodedao@gmail.com>
2018-11-16 16:22:54 +0700

    MVC-Lession16:16112018-1621: Using SessionFactory for connect DB and excute query., Manipulation with query Session session = sessionFactory.getCurrentSession()

--- SpringMVC/src/main/java/vn/com/spring/controller/HomeController.java
+++ SpringMVC/src/main/java/vn/com/spring/controller/HomeController.java
@@ -1,6 +1,13 @@
 package vn.com.spring.controller;
 
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,8 +18,21 @@ import vn.com.spring.entity.NhanVien;
 @Controller
 public class HomeController {
 
+	@Autowired
+	SessionFactory sessionFactory;
+
+	@Transactional
 	@RequestMapping(value = { "/", "/index" }, method = RequestMethod.GET)
-	public String index() {
+	public String index(ModelMap modelMap) {
+		String insertNV = "INSERT INTO NHANVIEN(EMAIL,HOTEN,MATKHAU) values('Cardinal', 'Tom B. Erichsen', 'Skagen 21')";
+		String query = "from NHANVIEN";
+		Session session = sessionFactory.getCurrentSession();
+		for(int i = 0; i< 1; i++) {
+			session.createNativeQuery(insertNV).executeUpdate();
+		}
+		
+		List<NhanVien> list = session.createQuery(query).getResultList();
+		modelMap.addAttribute("lists", list);
 		return "index";
 	}
 
