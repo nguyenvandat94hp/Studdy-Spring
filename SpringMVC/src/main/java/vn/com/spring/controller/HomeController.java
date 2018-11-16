928cb1f4a04008a5464043420be6917d3a807960
nguyenvandat94hp <doicodedao@gmail.com>
2018-11-16 09:56:49 +0700

    MVC-Lession14:16112018-0955: Away we are handler with @MaxtricVariable in controller

--- SpringMVC/src/main/java/vn/com/spring/controller/HomeController.java
+++ SpringMVC/src/main/java/vn/com/spring/controller/HomeController.java
@@ -2,6 +2,7 @@ package vn.com.spring.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.MatrixVariable;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,7 +16,7 @@ public class HomeController {
 	}
 
 	@RequestMapping(value = "/hello/{id}/{name}", method = RequestMethod.GET)
-	public String hello(@PathVariable("id") int id, @PathVariable("name") String name, ModelMap modeMap) {
+	public String hello(@PathVariable("id") int id, @MatrixVariable("name") String name, ModelMap modeMap) {
 		modeMap.addAttribute("id", id);
 		modeMap.addAttribute("name", name);
 		return "hello";
